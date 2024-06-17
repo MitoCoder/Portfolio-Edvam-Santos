6d5c6d3f9f22e031150025e3dd3f09c4f12b933b
Edvam S. (Nine_Nine) <edvamsantos1998@gmail.com>
2024-06-16 23:16:28 -0300

    Update importante

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,23 +1,29 @@
-import React, { useState, useEffect } from 'react';
-import { Layout, Avatar, Menu } from 'antd';
-import { GithubOutlined, LinkedinOutlined, FacebookOutlined, WhatsAppOutlined, InstagramOutlined } from '@ant-design/icons';
+import React, { useState, useEffect } from "react";
+import { Layout, Avatar, Menu } from "antd";
+import {
+  GithubOutlined,
+  LinkedinOutlined,
+  FacebookOutlined,
+  WhatsAppOutlined,
+  InstagramOutlined,
+} from "@ant-design/icons";
 
 const { Header } = Layout;
 const { SubMenu } = Menu;
 
 const MeuHeader = () => {
-  const [profileImageUrl, setProfileImageUrl] = useState('');
+  const [profileImageUrl, setProfileImageUrl] = useState("");
   const [hoveredKey, setHoveredKey] = useState(null);
   const [isMobile, setIsMobile] = useState(window.innerWidth <= 768); // Define se a tela é considerada mobile
 
   useEffect(() => {
-    fetch('https://api.github.com/repos/MitoCoder/MitoCoder')
-      .then(response => response.json())
-      .then(data => {
+    fetch("https://api.github.com/repos/MitoCoder/MitoCoder")
+      .then((response) => response.json())
+      .then((data) => {
         setProfileImageUrl(data.owner.avatar_url); // Obtém a URL da imagem de perfil do GitHub
       })
-      .catch(error => {
-        console.error('Erro ao obter dados do GitHub:', error);
+      .catch((error) => {
+        console.error("Erro ao obter dados do GitHub:", error);
       });
   }, []);
 
@@ -26,40 +32,40 @@ const MeuHeader = () => {
   };
 
   useEffect(() => {
-    window.addEventListener('resize', handleResize);
-    return () => window.removeEventListener('resize', handleResize);
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
   }, []);
 
   const socialLinks = [
     {
-      href: 'https://www.linkedin.com/in/mycosmus/',
+      href: "https://www.linkedin.com/in/mycosmus/",
       icon: <LinkedinOutlined />,
-      key: 'linkedin',
-      label: 'LinkedIn',
+      key: "linkedin",
+      label: "LinkedIn",
     },
     {
-      href: 'https://www.facebook.com/mycosmus',
+      href: "https://www.facebook.com/mycosmus",
       icon: <FacebookOutlined />,
-      key: 'facebook',
-      label: 'Facebook',
+      key: "facebook",
+      label: "Facebook",
     },
     {
-      href: 'https://wa.me/5511957207168',
+      href: "https://wa.me/5511957207168",
       icon: <WhatsAppOutlined />,
-      key: 'whatsapp',
-      label: 'WhatsApp',
+      key: "whatsapp",
+      label: "WhatsApp",
     },
     {
-      href: 'https://www.github.com/MitoCoder',
+      href: "https://www.github.com/MitoCoder",
       icon: <GithubOutlined />,
-      key: 'github',
-      label: 'GitHub',
+      key: "github",
+      label: "GitHub",
     },
     {
-      href: 'https://www.instagram.com/mycosmus',
+      href: "https://www.instagram.com/mycosmus",
       icon: <InstagramOutlined />,
-      key: 'instagram',
-      label: 'Instagram',
+      key: "instagram",
+      label: "Instagram",
     },
   ];
 
@@ -67,17 +73,38 @@ const MeuHeader = () => {
     <>
       <Header style={headerStyle}>
         <div style={logoStyle}>
-          {profileImageUrl && <Avatar src={profileImageUrl} size="large" style={{ marginRight: '6px' }} />} {/* Margem de 6px à direita do avatar */}
-          <h1 style={{ ...titleStyle, fontSize: '22px' }}>Portfólio</h1> 
+          {profileImageUrl && (
+            <Avatar
+              src={profileImageUrl}
+              size="large"
+              style={{ marginRight: "6px" }}
+            />
+          )}{" "}
+          {/* Margem de 6px à direita do avatar */}
+          <h1 style={{ ...titleStyle, fontSize: "21px" }}>Portfólio</h1>{" "}
+          {/* Título do portfólio com estilo e tamanho de fonte 15px */}
         </div>
         {isMobile ? (
-          <Menu mode="horizontal" style={{ background: '#001529' }}>
-            <SubMenu title={<span style={{ color: '#fff', marginRight: '4px' }}>Redes Sociais</span>} popupClassName="customSubMenu">
+          <Menu mode="horizontal" style={{ background: "#001529" }}>
+            <SubMenu
+              title={
+                <span style={{ color: "#fff", marginRight: "4px" }}>
+                  Redes Sociais
+                </span>
+              }
+              popupClassName="customSubMenu"
+            >
               {socialLinks.map(({ href, icon, key, label }) => (
                 <Menu.Item key={key}>
-                  <a href={href} target="_blank" rel="noopener noreferrer" style={{ color: '#001529', marginLeft: '4px' }}>
+                  <a
+                    href={href}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    style={{ color: "#001529", padding: "4px 8px" }}
+                  >
                     {icon}
-                    <span style={{ marginLeft: '4px' }}>{label}</span> {/* Espaçamento de 4px entre o ícone e o texto */}
+                    <span style={{ marginLeft: "4px" }}>{label}</span>{" "}
+                    {/* Espaçamento de 4px entre o ícone e o texto */}
                   </a>
                 </Menu.Item>
               ))}
@@ -91,12 +118,17 @@ const MeuHeader = () => {
                 href={href}
                 target="_blank"
                 rel="noopener noreferrer"
-                style={{ ...socialLinkStyle, borderBottom: hoveredKey === key ? '4px solid white' : 'none', margin: '0 2px' }}
+                style={{
+                  ...socialLinkStyle,
+                  borderBottom: hoveredKey === key ? "4px solid white" : "none",
+                  margin: "0 2px",
+                }}
                 onMouseEnter={() => setHoveredKey(key)}
                 onMouseLeave={() => setHoveredKey(null)}
               >
                 {icon}
-                <span style={{ marginLeft: '4px' }}>{label}</span> {/* Espaçamento de 4px entre o ícone e o texto */}
+                <span style={{ marginLeft: "4px" }}>{label}</span>{" "}
+                {/* Espaçamento de 4px entre o ícone e o texto */}
               </a>
             ))}
           </div>
@@ -108,41 +140,41 @@ const MeuHeader = () => {
 };
 
 const headerStyle = {
-  background: '#001529',
-  display: 'flex',
-  justifyContent: 'space-between',
-  alignItems: 'center',
-  padding: '0 20px',
-  height: '64px',
-  boxSizing: 'border-box',
+  background: "#001529",
+  display: "flex",
+  justifyContent: "space-between",
+  alignItems: "center",
+  padding: "0 20px",
+  height: "64px",
+  boxSizing: "border-box",
 };
 
 const logoStyle = {
-  display: 'flex',
-  alignItems: 'center',
+  display: "flex",
+  alignItems: "center",
 };
 
 const titleStyle = {
-  color: '#fff',
+  color: "#fff",
   margin: 0,
-  whiteSpace: 'nowrap',
-  fontSize: '1.5rem', // Tamanho de fonte ajustado para versão mobile
+  whiteSpace: "nowrap",
+  fontSize: "1.5rem", // Tamanho de fonte ajustado para versão mobile
 };
 
 const socialLinksContainer = {
-  display: 'flex',
-  gap: '20px', // Espaçamento entre os ícones de redes sociais
+  display: "flex",
+  gap: "20px", // Espaçamento entre os ícones de redes sociais
 };
 
 const socialLinkStyle = {
-  color: '#fff',
-  display: 'flex',
-  alignItems: 'center',
+  color: "#fff",
+  display: "flex",
+  alignItems: "center",
 };
 
 const lineStyle = {
-  height: '10px',
-  background: '#000', // Cor da linha alterada para dark (#000)
+  height: "10px",
+  background: "#000", // Cor da linha alterada para dark (#000)
 };
 
 export default MeuHeader;
